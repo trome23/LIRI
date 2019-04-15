7b7dff69f08ed85378e1a1a3aee2d0b753200f92
trome23 <romennoodles23@yahoo.com>
2019-04-14 18:59:24 -0600

    cleaned up the code

--- liri.js
+++ liri.js
@@ -11,30 +11,28 @@ var input = process.argv[3];
 
 
 switch(command) {
-    case "spotify-this-song":
-        if (input === undefined || input === null) {
+    case "spotify-this-song": 
+        if (input === undefined) {
             input= "The Sign";
-        }    
-        spotify.search({ type: 'track,artist', query: input, limit: 10}, function(err, data) {
-            if (err) {
-            return console.log('Error occurred: ' + err);
+        }          
+        spotify.search({ type: 'track', query: input, limit: 5}) 
+        .then(function(response) {       
+            // console.log(response.tracks);    
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                console.log("Artists: " + response.tracks.items[i].artists[0].name); 
+                console.log("Song Title: "+ response.tracks.items[i].name); 
+                console.log("Song Preview: " + response.tracks.items[i].preview_url); 
+                console.log("Album: " + response.tracks.items[i].album.name);   
+                console.log("\n-------------");
+                    
             }
-            var itemArr = data.tracks.items;
-            var song = itemArr[0];
-            if(input === "The Sign") {
-                for (var i = 0; i < itemArr.length; i++) {
-                   if(itemArr[i].album.artists[0].name === "Ace of Base") {
-                       song=itemArr[i];
-                   }
-                  
-                }
-            }
-          
-            console.log(JSON.stringify(song.album.artists[0].name, null, 2)); 
-            console.log(JSON.stringify(song.name, null, 2)); 
-            console.log(JSON.stringify(song.preview_url, null, 2)); 
-            console.log(JSON.stringify(song.album.name, null, 2)); 
-        });
+        })          
+        .catch(function(err) {
+            console.log(err) 
+          });
+        
+        
 
         break;
+    
 }
